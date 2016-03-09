c71c81c03e8f9d2dab20da3a7c00e213d4ea4b3e
luliqin <404817171@qq.com>
2016-03-09 21:03:10 +0800

    this is my commit

---
+++ fuLi.java
@@ -0,0 +1,19 @@
+
+package fuli;
+
+import java.util.Scanner;
+
+public class fuLi {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("请输入本金");
+		int benjin = scanner.nextInt();
+		System.out.print("请输入年限");
+		int year = scanner.nextInt();
+		System.out.print("请输入利息");
+		int lixi = scanner.nextInt();
+		int lixihe;
+		
+
+	}
